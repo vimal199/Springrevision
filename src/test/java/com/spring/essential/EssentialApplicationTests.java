e145d2a3ceb1721632fd8a514c514741a26d19f9
Decodetalker <vimalbhardwaj8@gmail.com>
2025-03-03 22:43:31 +0530

    First Commit

---
+++ src/test/java/com/spring/essential/EssentialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.essential;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssentialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
