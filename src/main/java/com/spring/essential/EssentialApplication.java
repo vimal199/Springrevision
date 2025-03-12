c6c566c5a9338a2a0c03899d9a8d6895fc804b9c
Decodetalker <vimalbhardwaj8@gmail.com>
2025-03-13 00:20:38 +0530

    Updating maven

--- src/main/java/com/spring/essential/EssentialApplication.java
+++ src/main/java/com/spring/essential/EssentialApplication.java
@@ -1,14 +1,22 @@
 package com.spring.essential;
 
+import com.spring.essential.com.infosys.config.SpringConfig;
+import com.spring.essential.com.infosys.demo.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class EssentialApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(EssentialApplication.class, args);
+		//SpringApplication.run(EssentialApplication.class, args);
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfig.class);
+		Customer customer = applicationContext.getBean(Customer.class);
+		customer.setCustomerName("Alex");
+		System.out.println(customer.registerCustomer(customer.getCustomerName()));
 
 	}
 
